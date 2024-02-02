521d357003f72edbe1df3092d90d5821f40dacf6
Neeraj Bendkhale <87565643+neeraj4509@users.noreply.github.com>
2024-02-02 17:15:06 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIg72k7Xgo2z10Fnu6bV5asSat0M3xr54",
-    authDomain: "stockify-a0ce2.firebaseapp.com",
-    projectId: "stockify-a0ce2",
-    storageBucket: "stockify-a0ce2.appspot.com",
-    messagingSenderId: "568575660455",
-    appId: "1:568575660455:web:5f98b5f164704014ed2682",
-    measurementId: "G-L1EM4K70EB"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
